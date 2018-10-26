fea3abdfac0621b4fe37c381a7e155fdfcf6ddfc
Svetlin Simonyan <svetlin.s@gmail.com>
2018-10-26 18:25:00 +0300

    Whitespace

--- lib/dir_walker.ex
+++ lib/dir_walker.ex
@@ -7,7 +7,7 @@ defmodule DirWalker do
   use GenServer
 
   def start_link(path, opts \\ %{})
- 
+
   def start_link(list_of_paths, opts) when is_list(list_of_paths) do
     mappers = setup_mappers(opts)
     GenServer.start_link(__MODULE__, {list_of_paths, mappers})
@@ -27,7 +27,7 @@ defmodule DirWalker do
 
         iex> {:ok,d} = DirWalker.start_link "."
         {:ok, #PID<0.83.0>}
-        iex> DirWalker.next(d)                 
+        iex> DirWalker.next(d)
         ["./.gitignore"]
         iex> DirWalker.next(d)
         ["./_build/dev/lib/dir_walter/.compile.elixir"]
@@ -35,7 +35,7 @@ defmodule DirWalker do
         ["./_build/dev/lib/dir_walter/ebin/Elixir.DirWalker.beam",
          "./_build/dev/lib/dir_walter/ebin/dir_walter.app",
          "./_build/dev/lib/dir_walter/.compile.lock"]
-        iex> 
+        iex>
   """
   def next(iterator, n \\ 1) do
     GenServer.call(iterator, { :get_next, n })
@@ -49,7 +49,7 @@ defmodule DirWalker do
   end
 
   @doc """
-  Implement a stream interface that will return a lazy enumerable. 
+  Implement a stream interface that will return a lazy enumerable.
 
   ## Example
 
@@ -58,19 +58,19 @@ defmodule DirWalker do
   """
 
   def stream(path_list) do
-    Stream.resource( fn -> 
-                      {:ok, dirw} = DirWalker.start_link(path_list) 
+    Stream.resource( fn ->
+                      {:ok, dirw} = DirWalker.start_link(path_list)
                       dirw
                     end ,
-                    fn(dirw) -> 
+                    fn(dirw) ->
                       case DirWalker.next(dirw,1) do
                         data when is_list(data) -> {data, dirw }
                         _ -> {:halt, dirw}
                       end
                     end,
-                    fn(dirw) -> DirWalker.stop(dirw) end 
+                    fn(dirw) -> DirWalker.stop(dirw) end
       )
-  end 
+  end
 
   ##################
   # Implementation #
@@ -79,7 +79,7 @@ defmodule DirWalker do
   def init(path_list) do
     { :ok, path_list }
   end
-  
+
   def handle_call({:get_next, _n}, _from, state = {[], _}) do
     { :reply, nil, state}
   end
@@ -95,18 +95,18 @@ defmodule DirWalker do
   end
 
 
-  # If the first element is a list, then it represents a 
+  # If the first element is a list, then it represents a
   # nested directory listing. We keep it as a list rather
   # than flatten it in order to keep performance up.
 
   defp first_n([ [] | rest ], n, mappers, result)  do
     first_n(rest, n, mappers, result)
   end
-      
+
   defp first_n([ [first] | rest ], n, mappers, result)  do
     first_n([ first | rest ], n, mappers, result)
   end
-      
+
   defp first_n([ [first | nested] | rest ], n, mappers, result)  do
     first_n([ first | [ nested | rest ] ], n, mappers, result)
   end
@@ -119,21 +119,21 @@ defmodule DirWalker do
   defp first_n([ path | rest ], n, mappers, result) do
     stat = File.stat!(path)
     case stat.type do
-    :directory ->
-      first_n([files_in(path) | rest], 
-              n, 
-              mappers, 
-              mappers.include_dir_names.(mappers.include_stat.(path, stat), result))
+      :directory ->
+        first_n([files_in(path) | rest],
+          n,
+          mappers,
+          mappers.include_dir_names.(mappers.include_stat.(path, stat), result))
 
-    :regular ->
+      :regular ->
         if mappers.matching.(path) do
-        first_n(rest, n-1, mappers, [ mappers.include_stat.(path, stat) | result ])
-      else
-        first_n(rest, n, mappers, result)
-      end
+          first_n(rest, n-1, mappers, [ mappers.include_stat.(path, stat) | result ])
+        else
+          first_n(rest, n, mappers, result)
+        end
 
-    true ->
-      first_n(rest, n, mappers, result)
+      true ->
+        first_n(rest, n, mappers, result)
     end
   end
 
@@ -156,12 +156,12 @@ defmodule DirWalker do
     %{
       include_stat:
         one_of(opts[:include_stat],
-               fn (path, _stat) -> path end,    
+               fn (path, _stat) -> path end,
                fn (path, stat)  -> {path, stat} end),
 
       include_dir_names:
         one_of(opts[:include_dir_names],
-               fn (_path, result) -> result end,    
+               fn (_path, result) -> result end,
                fn (path, result)  -> [ path | result ] end),
       matching:
         one_of(!!opts[:matching],
